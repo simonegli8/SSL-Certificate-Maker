55a10666fe9dd7d03b8d4b13cb18d078cf1ca435
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-02-11 21:10:03 +0100

    Fix

--- PushToNuget.bat
+++ PushToNuget.bat
@@ -3,10 +3,10 @@ SET /p ApiKey=<NugetApiKey.txt
 cd nupkg
 del /Q *.*
 
-cd ..\BibleMarkdown
+cd ..
 dotnet pack
 
-cd ..\nupkg
+cd nupkg
 
 for /r %%i in (*.nupkg) do (
     dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate --timeout 1200
