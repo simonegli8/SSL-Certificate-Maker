c6bfacbe5901ec48af89ec2d950b583c6b989293
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-06-01 23:02:19 +0200

    Restored WinExe and attach to console

--- SSLCertificateMaker.Avalonia/Program.cs
+++ SSLCertificateMaker.Avalonia/Program.cs
@@ -1,43 +1,66 @@
 ﻿using Avalonia;
 using Avalonia.Dialogs;
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
-using System.Diagnostics;
 
-namespace SSLCertificateMaker.Avalonia
+namespace SSLCertificateMaker.Avalonia;
+
+internal class Program
 {
-    internal class Program
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static async Task Main(string[] args)
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static async Task Main(string[] args)
+        if (OSInfo.IsWindows)
         {
-            if (args.Contains("-debug"))
+            try
             {
-                Console.WriteLine("Wait for debugger to attach...");
-                while (!Debugger.IsAttached) Thread.Sleep(200);
-                Debugger.Break();
+                if (!HasConsole()) AttachConsole();
             }
-#if  PackAsTool
-            if (!GUIToolInstaller.Installer.Run(args, "SSL-Certificate-Maker", "cert", "SSL-Certificate-Maker, a cross platform GUI tool to manage self signed certificates."))
-            {
-                BuildAvaloniaApp()
-                .StartWithClassicDesktopLifetime(args);
-            }
-#else
-                BuildAvaloniaApp()
-                .StartWithClassicDesktopLifetime(args);
-#endif
+            catch { }
         }
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace();
+        if (args.Contains("-debug"))
+        {
+            Console.WriteLine("Wait for debugger to attach...");
+            while (!Debugger.IsAttached) Thread.Sleep(200);
+            Debugger.Break();
+        }
+#if  PackAsTool
+        if (!GUIToolInstaller.Installer.Run(args, "SSL-Certificate-Maker", "cert", "SSL-Certificate-Maker, a cross platform GUI tool to manage self signed certificates."))
+        {
+            BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        }
+#else
+            BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+#endif
     }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    static bool HasConsole()
+    {
+        return GetConsoleWindow() != IntPtr.Zero;
+    }
+
+    const int ATTACH_PARENT_PROCESS = -1;
+
+    [DllImport("kernel32.dll")]
+    static extern bool AttachConsole(int dwProcessId);
+
+    static bool AttachConsole() => AttachConsole(ATTACH_PARENT_PROCESS);
 }
