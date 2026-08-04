943255c2b0132722fb424813c557033efe939e4d
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-08-04 21:22:57 +0200

    Bugfix installer

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,4 +1,4 @@
-SET PackageVersion=4.1.3
+SET PackageVersion=4.1.5
 SET Configuration=Release
 
 del nupkg\SSLCertificateMaker*.nupkg
--- PushToNuget.bat
+++ PushToNuget.bat
@@ -6,4 +6,8 @@ for /r %%i in (*.nupkg) do (
     dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate --timeout 1200
 )
 
+for /r %%i in (*.snupkg) do (
+    dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate --timeout 1200
+)
+
 cd ..
